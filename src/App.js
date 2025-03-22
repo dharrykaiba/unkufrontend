a2f16c3d3239e8d3e81c16b139f5f00ca036a632
dharrykaiba <dharrykaiba@gmail.com>
2025-03-22 08:46:57 -0500

    page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 // src/App.js
 import React from "react";
-import AppRouter from "./routes/AppRouter";
+//import AppRouter from "./routes/AppRouter"; <AppRouter />
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <AppRouter />
+        <p> Hello</p>
       </header>
     </div>
   );
