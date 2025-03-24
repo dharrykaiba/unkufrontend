a0a2d10aa90854fd7526b4781921d579e4fae0ae
dharrykaiba <dharrykaiba@gmail.com>
2025-03-23 20:19:28 -0500

    page

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -5,11 +5,14 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Test from "../pages/Test";
 
+const NotFound = () => <h2>404 - Página no encontrada</h2>;
+
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/test" element={<Test />} />
+      <Route path="*" element={<NotFound />} />  {/* Maneja rutas incorrectas */}
     </Routes>
   );
 };
