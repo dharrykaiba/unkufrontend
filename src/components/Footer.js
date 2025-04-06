5307cfe7b909a4fcdc640ef5d4b23bbcbff4f74e
dharrykaiba <dharrykaiba@gmail.com>
2025-04-06 10:24:28 -0500

    pagina completa

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,94 @@
-import React from "react";
-import "../styles/components/Footer.css"; // Importar los estilos
+// src/components/Navbar.js
+import React, { useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+import { useCart } from "../context/CartContext";
+import LoginForm from "./LoginPopup";
+import UserMenu from "./UserMenu";
+import CartSidebar from "./CartSidebar";
+import { FaUser, FaShoppingCart, FaSearch } from "react-icons/fa";
+import { Link, useNavigate, useLocation } from "react-router-dom"; // Importar useLocation
+import HamburgerMenu from "./HamburgerMenu";
 
-const Footer = () => {
+import "../styles/components/Navbar.css"; // Para estilos adicionales
+
+const Navbar = () => {
+  const { isAuthenticated } = useAuth();
+  const { isCartOpen, toggleCart } = useCart();
+  const [showLogin, setShowLogin] = useState(false);
+  const [searchTerm, setSearchTerm] = useState(""); // Estado para el término de búsqueda
+  const navigate = useNavigate(); // Hook para redireccionar
+  const location = useLocation(); // Hook para obtener la ruta actual
+
+  const toggleLoginPopup = () => {
+    setShowLogin(!showLogin);
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault(); // Evitar el comportamiento por defecto del formulario
+    if (searchTerm.trim()) {
+      navigate(`/search?nombre=${encodeURIComponent(searchTerm.trim())}`); // Redirigir a Search.js con el término de búsqueda
+      setSearchTerm(""); // Limpiar el campo de búsqueda después de la redirección
+    }
+  };
+
   return (
-    <footer className="footer">
-      <div className="footer-content">
-        <p>&copy; {new Date().getFullYear()} Todos los derechos reservados.</p>
-        <div className="footer-links">
-          <a href="/politica-de-privacidad">Política de Privacidad</a>
-          <a href="/terminos-y-condiciones">Términos y Condiciones</a>
-          <a href="/contacto">Contacto</a>
-        </div>
-        <div className="footer-social">
-          <a href="https://facebook.com" aria-label="Facebook">
-            <i className="fab fa-facebook"></i>
-          </a>
-          <a href="https://twitter.com" aria-label="Twitter">
-            <i className="fab fa-twitter"></i>
-          </a>
-          <a href="https://instagram.com" aria-label="Instagram">
-            <i className="fab fa-instagram"></i>
-          </a>
-        </div>
+    <nav>
+      <div className="logo-container">
+        {/* Imagen a la izquierda */}
+        <Link to="/" className="navbar-logo-link">
+          <img
+            src="./path/logo/logovert.png"
+            alt="Logo"
+            className="navbar-logo"
+          />
+          {/* Nombre de la aplicación */}
+        </Link>
+        <HamburgerMenu />
       </div>
-    </footer>
+      {/* Campo de búsqueda (no mostrar si estamos en /search) */}
+      {location.pathname !== "/search" && ( // Verificar la ruta actual
+        <form onSubmit={handleSearch} className="search-container">
+          <input
+            type="text"
+            placeholder="Buscar productos..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="search-input"
+          />
+          <button type="submit" className="search-button">
+            <FaSearch size={18} />
+          </button>
+        </form>
+      )}
+      {/* Menú hamburguesa a la derecha */}
+      <header>
+        {/* Botón de carrito */}
+        <button
+          onClick={(e) => {
+            e.stopPropagation();
+            toggleCart();
+          }}
+          className="icon-button"
+          aria-label={isCartOpen ? "Cerrar carrito" : "Abrir carrito"}
+        >
+          <FaShoppingCart size={24} />
+        </button>
+
+        {/* Botón de login */}
+        {!isAuthenticated ? (
+          <div style={{ position: "relative", display: "inline-block" }}>
+            <button onClick={toggleLoginPopup} className="icon-button">
+              <FaUser size={24} />
+            </button>
+            {showLogin && <LoginForm onClose={toggleLoginPopup} />}
+          </div>
+        ) : (
+          <UserMenu setShowLogin={setShowLogin} />
+        )}
+      </header>
+      <CartSidebar /> {/* Sidebar del carrito */}
+    </nav>
   );
 };
 
-export default Footer;
+export default Navbar;
